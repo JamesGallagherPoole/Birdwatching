6580d6719d4e8c7010c11726827f84d984b03844
James Poole <jgaple@gmail.com>
2025-03-25 22:24:01 +0100

    Setup an munit test suite

---
+++ tests/tests.c
@@ -0,0 +1,47 @@
+#include "munit.h"
+
+static MunitResult
+test_boids(void)
+{
+    return MUNIT_OK;
+}
+
+/* Creating a test suite is pretty simple.  First, you'll need an
+ * array of tests: */
+static MunitTest test_suite_tests[] = {
+    {(char *)"/example/boids", test_boids, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}};
+
+/* Now we'll actually declare the test suite.  You could do this in
+ * the main function, or on the heap, or whatever you want. */
+static const MunitSuite test_suite = {
+    /* This string will be prepended to all test names in this suite;
+     * for example, "/example/rand" will become "/µnit/example/rand".
+     * Note that, while it doesn't really matter for the top-level
+     * suite, NULL signal the end of an array of tests; you should use
+     * an empty string ("") instead. */
+    (char *)"",
+    /* The first parameter is the array of test suites. */
+    test_suite_tests,
+    /* In addition to containing test cases, suites can contain other
+     * test suites.  This isn't necessary in this example, but it can be
+     * a great help to projects with lots of tests by making it easier
+     * to spread the tests across many files.  This is where you would
+     * put "other_suites" (which is commented out above). */
+    NULL,
+    /* An interesting feature of µnit is that it supports automatically
+     * running multiple iterations of the tests.  This is usually only
+     * interesting if you make use of the PRNG to randomize your tests
+     * cases a bit, or if you are doing performance testing and want to
+     * average multiple runs.  0 is an alias for 1. */
+    1,
+    /* Just like MUNIT_TEST_OPTION_NONE, you can provide
+     * MUNIT_SUITE_OPTION_NONE or 0 to use the default settings. */
+    MUNIT_SUITE_OPTION_NONE};
+
+int main(int argc, char *argv[MUNIT_ARRAY_PARAM(argc + 1)])
+{
+    /* Finally, we'll actually run our test suite!  That second argument
+     * is the user_data parameter which will be passed either to the
+     * test or (if provided) the fixture setup function. */
+    return munit_suite_main(&test_suite, (void *)"µnit", argc, argv);
+}
